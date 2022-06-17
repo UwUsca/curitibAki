1c01e4ed83fa11489d5f3848df80de23dbfd909a
UwUsca <lucascordeirodelima12@gmail.com>
2022-06-17 18:20:49 -0300

    Finalização pag/card emergencia

--- src/pages/Emergencia.js
+++ src/pages/Emergencia.js
@@ -1,11 +1,22 @@
 import React from "react";
 import Logo from "../components/Logo";
-
+import CardEmergencia from "../components/Emergencia/CardEmergencia";
+import pText from "../components/Pontos/pText";
 
 function Emergencia(){
     return(
-        <div className="divBackground">
+        <div className="divBackground" style={{marginTop:"1%"}}>
             <Logo/>
+            <div className="divButtonsAreaPontos">
+                {pText("saúde")}
+                {CardEmergencia("Samu")}
+                {CardEmergencia("Bombeiros")}
+                {CardEmergencia("DefCivil")}
+                {pText("Segurança")}
+                {CardEmergencia("PolCivil")}
+                {CardEmergencia("PolMilitar")}
+                {CardEmergencia("PolRodoviaria")}
+            </div>
         </div>
     )
 }
